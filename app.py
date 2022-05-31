3bb2648dab67e867eba4937bbbfda0147631b547
Andrei Damian <Andrei Damian>
2022-05-31 08:18:08 -0400

    change message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def hello():
-    return "Hello world. I just created my second image"
+    return "Hello, this is a demo. 31st may 2022"
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
